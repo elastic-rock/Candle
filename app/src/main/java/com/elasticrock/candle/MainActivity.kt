d8204bf90e0b2d8bd6ab1162c0260475ea86c305
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-11-21 21:09:46 +0100

    Replace modal sheet with scaffold

--- app/src/main/java/com/elasticrock/candle/MainActivity.kt
+++ app/src/main/java/com/elasticrock/candle/MainActivity.kt
@@ -9,7 +9,6 @@ import android.view.Window
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,23 +18,22 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Brightness6
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
@@ -95,53 +93,44 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TorchApp(window: Window, dataStore: DataStore<Preferences>) {
-    val sheetState = rememberModalBottomSheetState()
+    val scaffoldState = rememberBottomSheetScaffoldState()
     val scope = rememberCoroutineScope()
-    var showBottomSheet by remember { mutableStateOf(true) }
 
-    Scaffold {paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.Bottom
-        ) {
-
-        }
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 128.dp,
+        containerColor = Color.White,
+        sheetContent = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .safeContentPadding(),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Row(
+                Icon(
+                    imageVector = Icons.Filled.Brightness6,
+                    contentDescription = null,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .safeContentPadding(),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Brightness6,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(start = 8.dp, end = 16.dp)
-                            .size(24.dp),
-                    )
-                    var brightness by remember { mutableFloatStateOf(runBlocking { DataStore(dataStore).readPreviousBrightness() }) }
-                    Slider(
-                        modifier = Modifier.padding(end = 8.dp),
-                        value = brightness,
-                        valueRange = minBrightness..maxBrightness,
-                        onValueChange = {
-                            brightness = it
-                            setBrightness(window, it)
-                            runBlocking { DataStore(dataStore).savePreviousBrightness(it) }
-                        }
-                    )
-                }
+                        .padding(start = 8.dp, end = 16.dp)
+                        .size(24.dp),
+                )
+                var brightness by remember { mutableFloatStateOf(runBlocking { DataStore(dataStore).readPreviousBrightness() }) }
+                Slider(
+                    modifier = Modifier.padding(end = 8.dp),
+                    value = brightness,
+                    valueRange = minBrightness..maxBrightness,
+                    onValueChange = {
+                        brightness = it
+                        setBrightness(window, it)
+                        runBlocking { DataStore(dataStore).savePreviousBrightness(it) }
+                    }
+                )
+            }
+        },
+        content = {
+            Column(modifier = Modifier.fillMaxSize()) {
+
             }
         }
-    }
+    )
 }
