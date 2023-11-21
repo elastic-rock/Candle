3bbf8ab5ce3ae47dec81479de2dbfa98c6fffb78
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-11-21 20:38:55 +0100

    Add dark theme, remove dynamic colour scheme and move brightness slider to bottom sheet in preparation for setting custom colour

--- app/src/main/java/com/elasticrock/candle/MainActivity.kt
+++ app/src/main/java/com/elasticrock/candle/MainActivity.kt
@@ -19,18 +19,23 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Brightness6
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
@@ -87,43 +92,55 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-private const val horizontal = 8
-private const val vertical = 32
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TorchApp(window: Window, dataStore: DataStore<Preferences>) {
-    Surface(
-        modifier = Modifier.safeContentPadding(),
-        color = Color.Transparent
-    ) {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(true) }
+
+    Scaffold {paddingValues ->
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             verticalArrangement = Arrangement.Bottom
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal.dp, vertical.dp),
-                verticalAlignment = Alignment.CenterVertically,
+
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState
             ) {
-                Icon(
-                    imageVector = Icons.Filled.Brightness6,
-                    contentDescription = null,
+                Row(
                     modifier = Modifier
-                        .padding(start = 8.dp, end = 16.dp)
-                        .size(24.dp),
-                )
-                var brightness by remember { mutableFloatStateOf(runBlocking { DataStore(dataStore).readPreviousBrightness() }) }
-                Slider(
-                    modifier = Modifier.padding(end = 8.dp),
-                    value = brightness,
-                    valueRange = minBrightness..maxBrightness,
-                    onValueChange = {
-                        brightness = it
-                        setBrightness(window, it)
-                        runBlocking { DataStore(dataStore).savePreviousBrightness(it) }
-                    }
-                )
+                        .fillMaxWidth()
+                        .safeContentPadding(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Brightness6,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 16.dp)
+                            .size(24.dp),
+                    )
+                    var brightness by remember { mutableFloatStateOf(runBlocking { DataStore(dataStore).readPreviousBrightness() }) }
+                    Slider(
+                        modifier = Modifier.padding(end = 8.dp),
+                        value = brightness,
+                        valueRange = minBrightness..maxBrightness,
+                        onValueChange = {
+                            brightness = it
+                            setBrightness(window, it)
+                            runBlocking { DataStore(dataStore).savePreviousBrightness(it) }
+                        }
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
+++ app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
@@ -2,7 +2,9 @@ package com.elasticrock.candle.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -15,37 +17,28 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 @Composable
 fun CandleTheme(
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            dynamicLightColorScheme(context)
-        }
-
+        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
