7ae4e1982b2d1ef7698e002854a93c7a17e4bfe2
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-12-08 18:28:16 +0100

    Add hue slider

--- app/src/main/java/com/elasticrock/candle/MainActivity.kt
+++ app/src/main/java/com/elasticrock/candle/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Brightness6
+import androidx.compose.material.icons.filled.Palette
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.datastore.core.DataStore
@@ -94,31 +96,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TorchApp(window: Window, dataStore: DataStore<Preferences>) {
     val scaffoldState = rememberBottomSheetScaffoldState()
+    var selectedHue by remember { mutableFloatStateOf(0f) }
     val scope = rememberCoroutineScope()
 
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
-        sheetPeekHeight = 128.dp,
-        containerColor = Color.White,
+        containerColor = Color.hsl(hue = selectedHue, saturation = 1f, lightness = 0.5f),
         sheetContent = {
-            Row(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .safeContentPadding(),
-                verticalAlignment = Alignment.CenterVertically,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Icon(
-                    imageVector = Icons.Filled.Brightness6,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .padding(start = 8.dp, end = 16.dp)
-                        .size(24.dp),
+                PreferenceSlider(
+                    icon = Icons.Filled.Palette,
+                    range = 0f..360f,
+                    value = selectedHue,
+                    onValueChange = { selectedHue = it }
                 )
                 var brightness by remember { mutableFloatStateOf(runBlocking { DataStore(dataStore).readPreviousBrightness() }) }
-                Slider(
-                    modifier = Modifier.padding(end = 8.dp),
+                PreferenceSlider(
+                    range = minBrightness..maxBrightness,
+                    icon = Icons.Filled.Brightness6,
                     value = brightness,
-                    valueRange = minBrightness..maxBrightness,
                     onValueChange = {
                         brightness = it
                         setBrightness(window, it)
@@ -128,9 +129,36 @@ fun TorchApp(window: Window, dataStore: DataStore<Preferences>) {
             }
         },
         content = {
-            Column(modifier = Modifier.fillMaxSize()) {
 
-            }
         }
     )
+}
+
+@Composable
+fun PreferenceSlider(
+    icon: ImageVector,
+    range: ClosedFloatingPointRange<Float>,
+    value: Float,
+    onValueChange: (Float) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 8.dp, end = 16.dp)
+                .size(24.dp),
+        )
+
+        Slider(
+            modifier = Modifier.padding(end = 8.dp),
+            value = value,
+            valueRange = range,
+            onValueChange = onValueChange
+        )
+    }
 }
