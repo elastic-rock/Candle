d8b28fa761338f8db275692cf5d1fdc00f0d4a85
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2023-12-27 11:54:56 +0100

    Fix colour scheme

--- app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
+++ app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
@@ -9,20 +9,17 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.Black
 )
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.White
 )
 
 @Composable
