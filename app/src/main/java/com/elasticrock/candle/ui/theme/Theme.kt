74f77a44d783d5f3e212094187be661f029ced5e
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-05-27 21:21:35 +0200

    Remove unused import directives

--- app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
+++ app/src/main/java/com/elasticrock/candle/ui/theme/Theme.kt
@@ -1,18 +1,11 @@
 package com.elasticrock.candle.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = Color.Black
