a1b77006881685529c360a184a80d723ea029419
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-09-09 19:52:19 +0200

    Stop advertising QSTile

--- app/src/main/java/com/elasticrock/candle/MainActivity.kt
+++ app/src/main/java/com/elasticrock/candle/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.elasticrock.candle
 
 import android.app.Activity
-import android.app.StatusBarManager
-import android.content.ComponentName
 import android.content.pm.ActivityInfo
 import android.os.Build
 import android.os.Bundle
@@ -33,15 +31,6 @@ class MainActivity : ComponentActivity() {
             CandleTheme {
                 TorchApp(this)
             }
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            val statusBarService = applicationContext.getSystemService(StatusBarManager::class.java)
-            statusBarService.requestAddTileService(
-                ComponentName(applicationContext, QSTileService::class.java.name),
-                applicationContext.getString(R.string.candle),
-                android.graphics.drawable.Icon.createWithResource(applicationContext,R.drawable.rounded_candle_qs),
-                {}) {}
         }
 
         this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED
