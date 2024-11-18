94e7f00e3c1fa07f657fb36690b6c3e097a1af99
David Weis <hi@davidweis.eu>
2024-11-18 23:22:14 +0100

    Restructure TileService

--- app/src/main/java/com/elasticrock/candle/QSTileService.kt
+++ app/src/main/java/com/elasticrock/candle/QSTileService.kt
@@ -1,10 +1,13 @@
 package com.elasticrock.candle
 
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.os.Build
 import android.service.quicksettings.TileService
+import android.util.Log
 
 class QSTileService : TileService() {
 
@@ -12,8 +15,8 @@ class QSTileService : TileService() {
         super.onStartListening()
         qsTile.label = getString(R.string.candle)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            val intent = Intent(this, MainActivity::class.java)
-            qsTile.activityLaunchForClick = PendingIntent.getActivity(applicationContext, 1, intent, PendingIntent.FLAG_IMMUTABLE + PendingIntent.FLAG_UPDATE_CURRENT)
+            // Ensure proper handling after update to 1.7.1
+            qsTile.activityLaunchForClick = null
         }
         qsTile.updateTile()
     }
@@ -21,7 +24,10 @@ class QSTileService : TileService() {
     override fun onClick() {
         super.onClick()
         val intent = Intent(this, MainActivity::class.java)
-        if (isLocked) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val pendingIntent = PendingIntent.getActivity(applicationContext, 1, intent, FLAG_IMMUTABLE + FLAG_UPDATE_CURRENT)
+            startActivityAndCollapse(pendingIntent)
+        } else if (isLocked) {
             startActivity(intent.addFlags(FLAG_ACTIVITY_NEW_TASK))
         } else {
             intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
